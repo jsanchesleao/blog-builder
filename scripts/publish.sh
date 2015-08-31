255661c972fdfab57a6fd36c69d39a51e2867a04
Jeferson Leão <jeferson.leao@b2wdigital.com>
2015-08-30 21:37:21 -0300

    new publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@ cd content && git pull
 cd ..
 
 echo "Cleaning up"
-cd public && git pull
+cd public && git pull && rm -rf public/*
 cd ..
 
 echo "Building blog"
@@ -12,6 +12,6 @@ node src/build.js
 echo "Publishing"
 cd public && git add --all && git commit -m "$(date)" && git push
 
-cd .. && git stash && git add public/ && git commit -m "$(date)" && git stash pop && git push
+cd .. && git add public/ && git commit -m "$(date)" && git push
 
 echo "Done :)"
